f3ccd151ce5ab557ec26d22037f1cef44ca8216d
Fero.Feroov <feroovt@gmail.com>
2022-01-09 11:24:09 +0100

    Keyboard and Mouse listeners etc

---
+++ src/com/feroov/main/inputs/KeyboardListener.java
@@ -0,0 +1,23 @@
+package com.feroov.main.inputs;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyboardListener implements KeyListener
+{
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
